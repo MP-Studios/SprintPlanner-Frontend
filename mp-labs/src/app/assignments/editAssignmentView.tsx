4d92995a59971123f54a25d6d1c770bfa975e748
PhilKononenko01 <phkon007@gmail.com>
2025-06-27 22:22:05 -0600

    working on alternate view file not functional yet

--- mp-labs/src/app/assignments/editAssignmentView.tsx
+++ mp-labs/src/app/assignments/editAssignmentView.tsx
@@ -8,5 +8,31 @@ type Assignment = {
     TaskDetails: string;
   };
 
+export default function AlternateView() {
+    const [assignments, setAssignments] = useState<Assignment[]>([]);
+    const [error, setError] = useState<string | null>(null);
 
+    useEffect(() => {
+        fetchAssignments();
+      }, []);
+
+    const fetchAssignments = async () => {
+        try {
+          const res = await fetch('http://localhost:8080/api/assignments');
+          if (!res.ok) throw new Error('Failed to fetch');
+          const data = await res.json();
+          setAssignments(data);
+        } catch (err) {
+          console.error(err);
+          setError('Error fetching assignments');
+        }
+      }; 
+
+      return (
+        <div className="editAssignment p-6 bg-white shadow-lg overflow-hidden h-screen flex flex-col">
+            <h1 className="text-xl font-semibold mb-4">Assignments</h1>
+            
+        </div>
+      );
+}
   
