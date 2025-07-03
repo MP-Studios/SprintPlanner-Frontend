7fbfd5103ca277a9a71e751e1e7064d9b2ae8dca
Miguel Wills <Miguelcow1@gmail.com>
2025-07-02 22:03:21 -0600

    Added script to run the backend and instructions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SprintPlanner-Frontend
+# run dispatch.ps1 to start up the backend in a seperate powershell terminal
---
+++ dispatch.ps1
@@ -0,0 +1,4 @@
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+cd ..\SprintPlanner-Backend\JavaCode\BackendCalling\
+Write-Host "We have moved directories and now we will run the shit"
+mvn spring-boot:run
