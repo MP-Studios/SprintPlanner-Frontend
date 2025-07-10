e5fe8e30a45a569b4d193a303b7fd15a1f419079
PhilKononenko01 <phkon007@gmail.com>
2025-07-10 09:47:23 -0600

    added supabase library support

--- mp-labs/src/app/assignments/editAssignmentView.tsx
+++ mp-labs/src/app/assignments/editAssignmentView.tsx
@@ -1,5 +1,9 @@
 'use client';
 import { useEffect, useState, FormEvent, ChangeEvent } from 'react';
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
 
 
 type Assignment = {
