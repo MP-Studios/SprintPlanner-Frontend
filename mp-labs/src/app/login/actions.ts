aa1b56cd2fe642922cf557cccaa261135d68a28a
PhilKononenko01 <phkon007@gmail.com>
2025-08-23 17:25:03 -0600

    added console logs for signup PLEASE READ CONSOLE WHEN SIGNING UP

--- mp-labs/src/app/login/actions.ts
+++ mp-labs/src/app/login/actions.ts
@@ -38,6 +38,8 @@ export async function signup(formData: FormData) {
   const { error } = await supabase.auth.signUp(data)
 
   if (error) {
+    console.error('signUp error:', error)
+    return { ok: false, message: error.message }
     redirect('/error')
   }
 
