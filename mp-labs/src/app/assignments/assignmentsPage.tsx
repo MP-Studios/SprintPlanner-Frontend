3f0542045a83d343cecb4b5b927021791c28dd58
Lina Hegazy <adhamhegazy@Adhams-MacBook-Air.local>
2025-09-09 18:30:39 -0600

    added the new button for assignment button.... waiting to be integrated with/connected to view

--- mp-labs/src/app/assignments/assignmentsPage.tsx
+++ mp-labs/src/app/assignments/assignmentsPage.tsx
@@ -8,13 +8,21 @@ export default function AssignmentContainer() {
 
     return (
         <div className="assignment p-6 bg-white shadow-lg h-screen flex flex-col">
-            <div className="flex items-center justify-between mb-4">
-                <button onClick={() => setShowAlternativeView(v => !v)} className="mt-auto bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
-                    {showAlternativeView ? 'Edit Assignments' : 'Create Assignments'}
-                </button>
+            <div className="flex items-center justify-end mb-4">
+            <label className="collection">
+                <input type="checkbox" />
+                <div>
+                    <span></span>
+                </div>
+            </label>
+            <a className="dribbble" 
+            href="https://dribbble.com/shots/4788602-Add-to-Collection-Animation" target="_blank">
+                <img src="https://cdn.dribbble.com/assets/dribbble-ball-1col-dnld-e29e0436f93d2f9c430fde5f3da66f94493fdca66351408ab0f96e2ec518ab17.png" 
+                alt="" />
+            </a>
             </div>
             <div className="flex-grow mb-4">
-                {showAlternativeView ? <AssignmentsPage/> : <EditAssignments/>}
+                <AssignmentsPage/>
             </div>
         </div>
     );
