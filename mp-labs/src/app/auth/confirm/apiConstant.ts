bb8ee666e3bed8ecfb8ccc2469892ef506973bb3
Miguel Wills <miguelcow1@gmail.com>
2025-09-28 14:14:28 -0600

    fixed fetchSprints.ts

--- mp-labs/src/app/auth/confirm/apiConstant.ts
+++ mp-labs/src/app/auth/confirm/apiConstant.ts
@@ -3,7 +3,7 @@ const API_BASE = process.env.API_BASE;
 // TODO have these strings be arguments that are either the dev or local host
 export const getDailyAssignments=`${API_BASE}/api/api/assignments/daily`;
 export const getSprintAssignments=`${API_BASE}/assignments`;
-export const getBackLog=`${API_BASE}/api/api/assignments/backlog`;
+export const getBackLog=`${API_BASE}/api/assignments/backlog`;
 export const saveAssignments = `${API_BASE}/api/supabase/saveAssignmentView`;
 
 
--- mp-labs/src/app/fetchLogic/fetchSprint.ts
+++ mp-labs/src/app/fetchLogic/fetchSprint.ts
@@ -1,9 +1,18 @@
 import {getSprintAssignments}  from "../auth/confirm/apiConstant"
-
+type Assignment = {
+  className: string;
+  name: string;
+  dueDate: string;
+  taskDetails: string;
+};
 
 export default async function FetchCurrentSprint()  {
     const response = await fetch(getSprintAssignments);
     if (!response.ok) throw Error("Unable To Get Current Sprint...");
+    
+    const data : Assignment[] = await response.json();
+    return data;
+}
     
 
 
