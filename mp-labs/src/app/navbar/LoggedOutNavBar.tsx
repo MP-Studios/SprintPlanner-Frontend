1143a6fb196165c9ba71f8ed612e24fc8e0d7d0f
bballdrake <118339213+bballdrake@users.noreply.github.com>
2025-10-24 12:39:32 -0600

    made the sign in page look a bit better (#43)
    Co-authored-by: drakebaldwin <dbaldwin1@mines.edu>

--- mp-labs/src/app/navbar/LoggedOutNavBar.tsx
+++ mp-labs/src/app/navbar/LoggedOutNavBar.tsx
@@ -1,13 +1,14 @@
-// components/LoggedOutNavBar.tsx
 import Link from "next/link";
 
 export default function LoggedOutNavBar() {
   return (
-    <nav className="navbar">
-      <Link href="/">Home</Link>
-      <Link href="/timer">Timer</Link>
-      <div className="navbar-right">
-      </div>
+    <nav className="navbar flex justify-center items-center py-4 bg-white shadow-md">
+      <Link 
+        href="/" 
+        className="text-3xl font-bold text-[#edf7f2] font-logo"
+      >
+        Planlli
+      </Link>
     </nav>
   );
 }
