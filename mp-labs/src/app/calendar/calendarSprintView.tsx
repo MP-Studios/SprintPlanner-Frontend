56edf07942018a7ce17456c66fe5c0114f932363
bballdrake <118339213+bballdrake@users.noreply.github.com>
2025-11-07 08:27:39 -0700

    fix time format in modal (#61)
    * fixed the date formatting
    
    * undid unnecessary change
    
    ---------
    
    Co-authored-by: drakebaldwin <dbaldwin1@mines.edu>

--- mp-labs/src/app/calendar/calendarSprintView.tsx
+++ mp-labs/src/app/calendar/calendarSprintView.tsx
@@ -339,7 +339,7 @@ export default function Calendar(){
             <div>
               <label className="block text-sm font-medium mb-1 text-black">Due Date</label>
               <input
-                type="date"
+                type="datetime-local"
                 name="dueDate"
                 value={formatDateTimeLocal(formData.dueDate)}
                 onChange={handleChange}
