f571ea5585229d90bade9cd385160ab99d325ba3
bballdrake <118339213+bballdrake@users.noreply.github.com>
2025-11-20 20:32:40 -0700

    Completed button redo (#71)
    * adding buttons
    
    * finished the pretty button
    
    ---------
    
    Co-authored-by: drakebaldwin <dbaldwin1@mines.edu>

--- mp-labs/src/app/assignments/editAssignmentView.tsx
+++ mp-labs/src/app/assignments/editAssignmentView.tsx
@@ -306,15 +306,24 @@ export default function EditAssignments() {
                       </div>
                       {isHovered && (
                         <div className="flex flex-col gap-2 ml-4">
-                          <button
+                          <div
+                            className="checkbox-wrapper-31"
+                            style={{ left: '6px' }}
                             onClick={(e) => {
                               e.stopPropagation();
                               markAsDone(index);
                             }}
-                            className="globalButton bg-gray-300 px-2 py-1 rounded text-sm whitespace-nowrap"
                           >
-                            {isDone ? "Undo" : "Completed!"}
-                          </button>
+                            <input type="checkbox" checked={isDone} readOnly />
+                            <svg viewBox="0 0 35.6 35.6">
+                              <circle className="background" cx="17.8" cy="17.8" r="17.8"></circle>
+                              <circle className="stroke" cx="17.8" cy="17.8" r="14.37"></circle>
+                              <polyline
+                                className="check"
+                                points="11.78 18.12 15.55 22.23 25.17 12.87"
+                              ></polyline>
+                            </svg>
+                          </div>
                           <div 
                             className="delete-container"
                             onClick={(e) => e.stopPropagation()}
